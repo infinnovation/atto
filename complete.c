204ba5abee4247e8bd516cf77eca0284c2abeefd
Colin Hogben <colin@infinnovation.co.uk>
2017-12-31 10:12:25 +0000

    complete: Alternate completion using readdir()
    In an embedded system, the technique of spawning an "echo" to obtain
    matching filenames may not apply.  An alternative completion scheme
    uses opendir() / readdir() to find matches.  There are differences
    in behaviour: (1) no generic wildcard expansion; (2) matching names
    are not returned in sorted order; (3) filenames containing spaces
    and shell metacharacters are handled correctly.

--- complete.c
+++ complete.c
@@ -1,16 +1,25 @@
 /* complete.c, Atto Emacs, Hugh Barney, Public Domain, 2016 */
 
 #include "atto.h"
+#if ATTO_COMPLETE_READDIR
+#include <dirent.h>
+#endif
 
 /* basic filename completion, based on code in uemacs/PK */
 int getfilename(char *prompt, char *buf, int nbuf)
 {
+#if ATTO_COMPLETE_READDIR
+	DIR *dir = NULL;
+	struct dirent *entry;
+	int dpos, mlen, any = FALSE;
+#else
 	static char temp_file[] = TEMPFILE;
-	int cpos = 0;	/* current character position in string */
-	int k = 0, c, fd, didtry, iswild = 0;
-
+	int c, fd, iswild = 0;
 	char sys_command[255];
 	FILE *fp = NULL;
+#endif
+	int cpos = 0;	/* current character position in string */
+	int k = 0, didtry;
 	buf[0] ='\0';
 
 	for (;;) {
@@ -22,7 +31,11 @@ int getfilename(char *prompt, char *buf, int nbuf)
 		case 0x07: /* ctrl-g, abort */
 		case 0x0a: /* cr, lf */
 		case 0x0d:
+#if ATTO_COMPLETE_READDIR
+			if (dir) closedir(dir);
+#else
 			if (fp != NULL) fclose(fp);
+#endif
 			return (k != 0x07 && cpos > 0);
 
 		case 0x7f: /* del, erase */
@@ -37,6 +50,58 @@ int getfilename(char *prompt, char *buf, int nbuf)
 			break;
 
 		case 0x09: /* TAB, complete file name */
+#if ATTO_COMPLETE_READDIR
+			if (! didtry) {
+				if (dir)
+					closedir(dir);
+				dir = NULL;
+				// Find directory name to open
+				dpos = cpos;
+				while (dpos > 0 && buf[dpos-1] != '/')
+					-- dpos;
+				// Now either at start of buf or after last '/'
+				mlen = cpos - dpos; /* Length to match */
+			}
+			while (1) {
+				if (dir == NULL) {
+					// Directory to open, given buffer:
+					// "a/b"  -> "a"
+					// "/a/b" -> "/a"
+					// "/b"   -> "/"
+					// ""     -> "."
+					// "b"    -> "."
+					if (dpos == 0) {
+						dir = opendir(".");
+					} else if (dpos == 1) {
+						dir = opendir("/");
+					} else {
+						buf[dpos-1] = 0; // Was '/'
+						dir = opendir(buf);
+						buf[dpos-1] = '/';
+					}
+					any = FALSE;
+					if (! dir) break;
+				}
+				// Get next matching entry
+				entry = readdir(dir);
+				if (! entry) {
+#if ATTO_NO_REWINDDIR
+					closedir(dir);
+					dir = NULL;
+#else
+					rewinddir(dir);
+#endif
+					if (! any)
+						break;
+				} else if (! strncmp(entry->d_name, buf+dpos, mlen)) {
+					any = TRUE;
+					strncpy(buf+dpos, entry->d_name, nbuf-dpos-1);
+					buf[nbuf-1] = 0;
+					cpos = dpos + strlen(buf+dpos);
+					break;
+				}
+			}
+#else
 			/* scan backwards for a wild card and set */
 			iswild=0;
 			while (cpos > 0) {
@@ -69,7 +134,7 @@ int getfilename(char *prompt, char *buf, int nbuf)
 
 			buf[cpos] = '\0';
 			if (c != ' ') rewind(fp);
-			didtry = 1;
+#endif
 			break;
 
 		default:
